22d2d530af9404ae2ffa3ee64b8bff6222771c1c
samas3 <samas3@163.com>
2024-04-30 19:20:27 +0800

    update

--- dps.py
+++ dps.py
@@ -63,10 +63,12 @@ def calc(p, atk):
     tot2 = 0
     time = max(lap, maxreload)
     time = max(time, maxhealth * lap)
+    petaldmg = []
     for i, j in enumerate(lst):
         x = hitcount(j, time, atk, (i * clockwise % len(lst)) / len(lst) * lap)
         tot2 += x * j.damage
-    return lap, tot1, time, tot2
+        petaldmg.append([j, x * j.damage])
+    return lap, tot1, time, tot2, petaldmg
 t = Tk()
 t.title('Hornex.PRO dps计算')
 Label(t, text='花瓣', font=('', 15)).grid(row=0, column=0)
@@ -77,10 +79,18 @@ tar = Entry(t, font=('', 15))
 tar.grid(row=1, column=1)
 res = Label(t, font=('', 20))
 res.grid(row=3, column=0, columnspan=2)
+dmg = Text(t, font=('', 13), height=18, width=30)
+dmg.grid(row=4, column=0, columnspan=2)
 def do():
-    a, b, c, d = calc(pt.get().strip().split(), tar.get())
-    res['text'] = f'单圈时间: {round(a, 2)} s\n爆发输出: {int(b)}\n爆发dps: {int(b / a)}\n' \
+    a, b, c, d, e = calc(pt.get().strip().split(), tar.get())
+    e = sorted(e, key=lambda x:-x[1])
+    res['text'] = f'单圈时间: {round(a, 2)} s\n' \
     f'一轮时间: {round(c, 2)} s\n一轮输出: {int(d)}\n一轮dps: {int(d / c)}\n' \
-    f'预计击杀时间: {round(applyHealth(tar.get()[:2], tar.get()[2:]) / (d / c), 2)} s'
+    f'预计击杀时间: {round(applyHealth(tar.get()[:2], tar.get()[2:]) / (d / c), 2)} s\n' \
+    f'分别造成伤害: '
+    #f'爆发输出: {int(b)}\n爆发dps: {int(b / a)}\n' \
+    dmg.delete(1.0, 'end')
+    for i in e:
+        dmg.insert('end', f'{str(i[0])} {str(int(i[1]))}\n')
 Button(t, text='计算', command=do).grid(row=2, column=0, columnspan=2)
 t.mainloop()
